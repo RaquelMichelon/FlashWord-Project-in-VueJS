0a9565c886f4c72d90ebcee29b8dff1879091aa2
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-31 09:31:55 +0100

    added a new data property

--- flashword/app.js
+++ flashword/app.js
@@ -3,11 +3,14 @@
 
 //create an Options obj and assign to a constant, then pass that as an arg of createApp()
 const FlashWord = {
+    //vue data is reactive
     data() {
         return {
             //define the data properties here
             wordA: 'olá / oi',
-            wordB: 'hello'
+            wordB: 'hello',
+            //this will be filled by the user
+            answer: ''
         }
     }
 }
